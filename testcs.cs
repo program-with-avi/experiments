2f8c06bcd96dd627490e4a21c330ef5cd3af687d
Ag <omgavig@gmail.com>
2026-04-28 21:58:24 +0530

    Create testcs.cs

---
+++ testcs.cs
@@ -0,0 +1,12 @@
+using System
+namespace Projecttest
+{
+  class Printofpython
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World\nPress Enter to exit");
+      Console.Readline();
+    }
+  }
+}
